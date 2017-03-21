359c7d1051cd8da16192a207949da29368878245
Shawn Wilson <swilson1337@gmail.com>
2017-03-21 17:31:55 -0400

    Update SideNavAsset.php

--- src/SideNavAsset.php
+++ src/SideNavAsset.php
@@ -6,7 +6,7 @@ class SideNavAsset extends \yii\web\AssetBundle
 {
 	public $sourcePath = '@vendor/swilson1337/yii2-widget-sidenav/assets';
 	
-	public $css = [
-		'css/sidenav.css',
+	public $js = [
+		'js/sidenav.js',
 	];
 }
