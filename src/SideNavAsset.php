f2ec6a2b0664157335f5823a263c7ec7925a9b5f
Shawn Wilson <swilson1337@gmail.com>
2017-03-21 16:38:45 -0400

    Create SideNavAsset.php

---
+++ src/SideNavAsset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace swilson1337\sidenav;
+
+class SideNavAsset extends \yii\web\AssetBundle
+{
+	public $sourcePath = '@vendor/swilson1337/yii2-widget-sidenav/assets';
+	
+	public $css = [
+		'css/sidenav.css',
+	];
+}
