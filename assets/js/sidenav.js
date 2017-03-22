951ef707fd62c87ab0a59d62ba7f2a84d14cc113
Shawn Wilson <swilson1337@gmail.com>
2017-03-21 20:34:29 -0400

    Update sidenav.js

--- assets/js/sidenav.js
+++ assets/js/sidenav.js
@@ -17,4 +17,27 @@ $(document).on("ready", function()
 		
 		return false;
 	});
+	
+	$(".sidenav-toggle").on("click", function(e)
+	{
+		var el = $(this);
+		
+		var target = $("#" + el.data("target"));
+		
+		var toggleButton = $("#" + el.data("toggle"));
+		
+		if (target != null && !target.hasClass("collapsing"))
+		{
+			target.collapse("toggle");
+			
+			if (toggleButton != null)
+			{
+				toggleButton.toggleClass("glyphicon-chevron-up glyphicon-chevron-down");
+			}
+		}
+		
+		e.preventDefault();
+		
+		return false;
+	});
 });
